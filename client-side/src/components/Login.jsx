290b86a35fec888863ea669521f989730582683b
SBJ <sbj@aol.com>
2023-10-11 13:29:24 -0400

    updated login

--- client-side/src/components/Login.jsx
+++ client-side/src/components/Login.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 export function Login(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = async () => {
+        const raw = {
+            email,
+            password,
+        };
+    }
     return (
         <>
             <h2>Login</h2>
